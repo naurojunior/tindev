e922f4103e7cfb1826f0b748c4deb85d63ffe9d0
Nauro <nauro@outlook.com>
2019-08-20 03:18:51 -0300

    Add link

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link }  from 'react-router-dom';
 
 import logo from '../assets/logo.svg';
 import dislike from '../assets/dislike.svg';
@@ -47,7 +48,9 @@ export default function Main({ match }) {
 
     return (
         <div className="main-container">
-            <img src={logo} alt="Tindev" />
+            <Link to="/">
+                <img src={logo} alt="Tindev" />
+            </Link>
             {users.length > 0 ? (
                 <ul>
                     {users.map(user => (
