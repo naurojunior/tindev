9af92a23b41c4dbe1848c40960502080b8e35a8c
Nauro <nauro@outlook.com>
2019-08-20 02:49:31 -0300

    Add css

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,80 @@
 import React from 'react';
 
-export default function Main(){
+import logo from '../assets/logo.svg';
+import dislike from '../assets/dislike.svg';
+import like from '../assets/like.svg';
+
+import './Main.css';
+
+export default function Main({ match }){
     return (
-        <h1>Hellow</h1>
+        <div className="main-container">
+            <img src={logo} alt="Tindev" />
+            <ul>
+                
+            <li>
+                    <img src="https://www.medicalnewstoday.com/content//images/articles/322/322868/golden-retriever-puppy.jpg" alt="" />
+                    <footer>
+                        <strong>Nrj</strong>
+                        <p>Teste de Teste</p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button" >
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+                        <button type="button" >
+                            <img src={like} alt="Like" />
+                        </button>
+                    </div>
+                </li>
+                
+                <li>
+                    <img src="https://www.medicalnewstoday.com/content//images/articles/322/322868/golden-retriever-puppy.jpg" alt="" />
+                    <footer>
+                        <strong>Nrj</strong>
+                        <p>Teste de Teste</p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button" >
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+                        <button type="button" >
+                            <img src={like} alt="Like" />
+                        </button>
+                    </div>
+                </li>
+                
+                <li>
+                    <img src="https://www.medicalnewstoday.com/content//images/articles/322/322868/golden-retriever-puppy.jpg" alt="" />
+                    <footer>
+                        <strong>Nrj</strong>
+                        <p>Teste de Testeasd asudha usdhua shduash udasdh asudhausd uashdu ahsudahsu hdasudh</p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button" >
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+                        <button type="button" >
+                            <img src={like} alt="Like" />
+                        </button>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://www.medicalnewstoday.com/content//images/articles/322/322868/golden-retriever-puppy.jpg" alt="" />
+                    <footer>
+                        <strong>Nrj</strong>
+                        <p>Teste de Teste</p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button" >
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+                        <button type="button" >
+                            <img src={like} alt="Like" />
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div>
     );
 }
