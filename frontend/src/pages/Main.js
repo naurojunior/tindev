d8f1bcf77dd69ffdc8e2e48703ef0e87ccb442cb
Nauro <nauro@outlook.com>
2019-08-20 03:04:58 -0300

    Add list of users that came from api^

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import logo from '../assets/logo.svg';
 import dislike from '../assets/dislike.svg';
@@ -6,74 +6,46 @@ import like from '../assets/like.svg';
 
 import './Main.css';
 
-export default function Main({ match }){
+import api from '../services/api';
+
+export default function Main({ match }) {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function loadUsers() {
+            const response = await api.get('/devs', {
+                headers: {
+                    user: match.params.id
+                }
+            })
+
+            setUsers(response.data);
+        }
+
+        loadUsers();
+    }, [match.params.id]);
+
     return (
         <div className="main-container">
             <img src={logo} alt="Tindev" />
             <ul>
-                
-            <li>
-                    <img src="https://www.medicalnewstoday.com/content//images/articles/322/322868/golden-retriever-puppy.jpg" alt="" />
-                    <footer>
-                        <strong>Nrj</strong>
-                        <p>Teste de Teste</p>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button" >
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                        <button type="button" >
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div>
-                </li>
-                
-                <li>
-                    <img src="https://www.medicalnewstoday.com/content//images/articles/322/322868/golden-retriever-puppy.jpg" alt="" />
-                    <footer>
-                        <strong>Nrj</strong>
-                        <p>Teste de Teste</p>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button" >
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                        <button type="button" >
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div>
-                </li>
-                
-                <li>
-                    <img src="https://www.medicalnewstoday.com/content//images/articles/322/322868/golden-retriever-puppy.jpg" alt="" />
-                    <footer>
-                        <strong>Nrj</strong>
-                        <p>Teste de Testeasd asudha usdhua shduash udasdh asudhausd uashdu ahsudahsu hdasudh</p>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button" >
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                        <button type="button" >
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://www.medicalnewstoday.com/content//images/articles/322/322868/golden-retriever-puppy.jpg" alt="" />
-                    <footer>
-                        <strong>Nrj</strong>
-                        <p>Teste de Teste</p>
-                    </footer>
-                    <div className="buttons">
-                        <button type="button" >
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                        <button type="button" >
-                            <img src={like} alt="Like" />
-                        </button>
-                    </div>
-                </li>
+                {users.map(user => (
+                    <li key={ user._id }>
+                        <img src={ user.avatar } alt={ user.name } />
+                        <footer>
+                            <strong>{ user.name }</strong>
+                            <p>{ user.bio }</p>
+                        </footer>
+                        <div className="buttons">
+                            <button type="button" >
+                                <img src={dislike} alt="Dislike" />
+                            </button>
+                            <button type="button" >
+                                <img src={like} alt="Like" />
+                            </button>
+                        </div>
+                    </li>
+                ))}
             </ul>
         </div>
     );
